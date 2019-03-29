609c023b5c6c87b388a6b9ade46c5e11c128dd94
Morteza <Morteza.j8@gmail.com>
2019-03-29 16:23:50 +0430

    fcm web

---
+++ Readme.md
@@ -0,0 +1,26 @@
+#Example Of Firebase Cloud Messaging - Web Client (JS)
+
+</br></br></br></br>
+
+
+
+
+---
+
+Change the variable on File<b>`script.js`</b> 
+ by your <b>Massaging Sender Id</b>.
+ 
+ variable should be accessible via<a href="https://console.firebase.google.com/">Firebase Console</a>
+ 
+ 
+ ```javascript
+ var config = {
+     messagingSenderId: "551579329505"
+ };
+```
+***
+
+
+
+
+repository based on <a href="https://rakibul.net/fcm-web-js/"> Rakibul Islam Tutorial </a>
---
+++ firebase-messaging-sw.js
@@ -0,0 +1,22 @@
+// Give the service worker access to Firebase Messaging.
+importScripts('firebase-app.js')
+importScripts('firebase-messaging.js')
+
+// Initialize the Firebase app in the service worker by passing in the messagingSenderId.
+var config = {
+    messagingSenderId: "your_messaging_sender_id"
+};
+firebase.initializeApp(config);
+
+// Retrieve an instance of Firebase Data Messaging so that it can handle background messages.
+const messaging = firebase.messaging()
+messaging.setBackgroundMessageHandler(function(payload) {
+    const notificationTitle = 'Data Message Title';
+    const notificationOptions = {
+        body: 'Data Message body',
+        icon: 'alarm.png'
+    };
+
+    return self.registration.showNotification(notificationTitle,
+        notificationOptions);
+});
